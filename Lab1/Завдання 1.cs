b8f81e4d1a72868a5e147c346d6ee8a1859a92c0
allmallino <67687853+allmallino@users.noreply.github.com>
2021-10-19 23:47:03 +0300

    Бета лабораторной 1

---
+++ Lab1/Завдання 1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace Завдання_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("x = "); int x = int.Parse(ReadLine());
+            Write("y = "); int y = int.Parse(ReadLine());
+            Write("z = "); int z = int.Parse(ReadLine());
+            if (Sin(2 * x * y * z) == 0)
+            {
+                Write("Помилка при знаходженнi а");
+                return;
+            }
+            double a = (x * y * z) / Sin(2 * x * y * z);
+            if (Pow(a, 4) + Pow(Log(a + x * y), 1.0 / 3) == 0||a+x*y<=0)
+            {
+                Write("Помилка при знаходженнi b");
+                return;
+            }
+            double b = 1 / (Pow(a, 4) + Pow(Log(a + x * y), 1.0 / 3));
+            WriteLine("a = " + a);
+            WriteLine("b = " + b);
+            ReadLine();
+        }
+    }
+}
---
+++ Lab1/Завдання 2.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using static System.Console;
+using static System.Math;
+
+namespace Завдання_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Write("n = "); int n = int.Parse(ReadLine());
+            Write("m = "); int m = int.Parse(ReadLine());
+            for(int i = n; i<=m; i++)
+            {
+                int sum = 0;
+                for(int k =1; k < i; k++)
+                {
+                    if (i % k == 0)
+                        sum += k;
+                }
+
+                if (sum <= m && sum >= n)
+                {
+                    int sum2 = 0;
+                    for (int j = 1; j < sum; j++)
+                    {
+                        if (sum % j == 0)
+                            sum2 += j;
+                    }
+                    if(sum2==i)
+                        WriteLine("{0} i {1}",i,sum);
+                }
+            }
+
+        }
+    }
+}
